160fa1d52a941cc6590306d5ec5f60c805246312
ikey123 <94415427@qq.com>
2025-08-10 12:18:58 +0800

    Clarity 跟踪脚本

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,18 @@ export default function RootLayout({
             __html: JSON.stringify(jsonLd),
           }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ssh0n4e22g");
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {children}
